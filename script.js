63dbfed1f98dfa440885d99fdbdaed697bbc7c50
sandibekibraimov <sandibek@gmail.com>
2021-11-02 23:51:14 -0400

    added interval

--- script.js
+++ script.js
@@ -22,6 +22,15 @@ form.addEventListener('submit', (e) => {
 
   //   console.log(score);
   scrollTo(0, 0);
-  result.querySelector('span').textContent = `${score}%`;
   result.classList.remove('d-none');
+
+  let output = 0;
+  const timer = setInterval(() => {
+    result.querySelector('span').textContent = `${output}%`;
+    if (output === score) {
+      clearInterval(timer);
+    } else {
+      output++;
+    }
+  }, 10);
 });
